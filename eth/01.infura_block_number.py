0feeb3cc218766490e10e8ff919fec9957686e54
Whackur <ioumelon7@gmail.com>
2020-08-18 17:20:31 +0900

    Modify infura

--- eth/01.infura_block_number.py
+++ eth/01.infura_block_number.py
@@ -2,4 +2,6 @@ from web3 import Web3
 from conf import infura
 
 w3 = Web3(Web3.HTTPProvider(f'https://mainnet.infura.io/v3/{infura["project_id"]}'))
+
+print("Last BlockNumber:")
 print(w3.eth.blockNumber)
--- eth/02.infura_get_price.py
+++ eth/02.infura_get_price.py
@@ -7,10 +7,9 @@ url = f"https://mainnet.infura.io/v3/{project_id}"
 
 body = {
     "jsonrpc": "2.0",
-    "method": "eth_getTransactionByHash",
-    "params": ["0xbb3a336e3f823ec18197f1e13ee875700f08f03e2cab75f0d0b118dabb44cba0"],
+    "method": "eth_getBalance",
+    "params": ["0xc94770007dda54cF92009BFF0dE90c06F603a09f", "latest"],
     "id": 1,
 }
-
 response = requests.post(url, data=json.dumps(body))
 print(response.text)
